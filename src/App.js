9f2221c11a84ce79cf3e0c577c76cc839d08fb6c
akaorignal <torir.adams@gmail.com>
2017-12-21 08:02:27 -0700

    master

--- src/App.js
+++ src/App.js
@@ -3,16 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { value: ""};
+   //this.addItem = new ItemService();
+
+    this.change = this.change.bind(this);
+    this.handle = this.handle.bind(this);//dashboard.handle 
+}
+
+//functions
+change(event){
+    this.setState({ value: event.target.value});
+}
+
+handle(event){
+    event.preventDefault();
+    this.setState({ value: event.target.value});
+    
+    this.addItem.sendData(this.state.value);
+    this.props.history.push('/');
+}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">everybody loves chores</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <form onSubmit={this.handle}>
+          <label>
+            Username:
+            
+            <input type="text" value={this.state.value} onChange={this.change} className="form-control"/>
+          </label><br/>
+          <label> Password:
+          <input type="text" value={this.state.value} onChange={this.change} className="form-control"/>
+          </label>< br/>
+          <input type="submit" value="Submit" className="btn btn-primary"/>
+        </form>
+        
       </div>
     );
   }
