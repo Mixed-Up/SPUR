40d91af91dd90250a6563e40724b555a72c2914e
perltech <noah.perlmutter@gmail.com>
2017-12-21 21:30:45 -0700

    add pseudocode for routes to create task and dashboard

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
+// Models
+// let models = require('./models');
+// const CreateTask = require('.models/CreateTask');
+// const NewChallenge = require('.models/NewChallenge');
+
 const PORT = 8080;
 const app = express();
 
@@ -12,7 +17,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Homepage'));
-console.log('GET sent');
+app.post('/', (req, res) => res.json('Login Info'));
+
+// app.get('/:username', (req,res) => Friends.findAll({})
+//     .then((friend) => {
+//         Challenges.findAll({})
+//         .then((challenge) => {
+//             res.send('Dashboard');
+//         });
+//     })
+// );
+
+
+// app.get('/:username/create-task', (req, res) => res.send('CreateTask'));
+// app.post('/:username/create-task', (req, res) => res.json(NewChallenge));
 
 mongoose.connect('mongodb://localhost/spur');
 
